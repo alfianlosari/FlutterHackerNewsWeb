3f8ea966a4e5132a1730c7088643740d028150e5
Alfian Losari <alfianlosari@gmail.com>
2019-05-30 05:09:07 +0700

    Add responsive

--- lib/comment_page.dart
+++ lib/comment_page.dart
@@ -26,61 +26,72 @@ class _CommentPageState extends State<CommentPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Comments (${widget.item.descendants})")),
-      body: (widget.item.kids.isEmpty)
-          ? ListView(
-              children: <Widget>[
-                HeaderWidget(widget.item),
-                Container(
-                  padding: EdgeInsets.all(32.0),
-                  child: Center(
-                    child: Text("There are no comments available",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500, fontSize: 17.0)),
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemCount: 1 + widget.item.kids.length,
-              itemBuilder: (BuildContext context, int position) {
-                if (position == 0) {
-                  return HeaderWidget(widget.item);
-                }
+    final double mediumBreakPoint = 800;
+    final smallestWidth = MediaQuery.of(context).size.shortestSide;
 
-                return FutureBuilder(
-                    future: getItemWithComments(widget.item.kids[position - 1]),
-                    builder: (BuildContext context, AsyncSnapshot snapshot) {
-                      if (comments[position - 1] != null) {
-                        var item = comments[position - 1];
-                        return CommentRow(
-                          item: item,
-                          key: Key(item.id.toString()),
-                        );
-                      }
-                      if (snapshot.hasData && snapshot.data != null) {
-                        var item = snapshot.data;
-                        comments[position - 1] = item;
-                        return CommentRow(
-                            item: item, key: Key(item.id.toString()));
-                      } else if (snapshot.hasError) {
-                        return Container(
-                          padding: EdgeInsets.all(16.0),
-                          child: Center(child: Text("Error Loading Item")),
-                        );
-                      } else {
-                        return Container(
-                          padding: EdgeInsets.all(32.0),
-                          child: Center(
-                            child: CircularProgressIndicator(),
-                          ),
-                        );
-                      }
-                    });
-              },
-            ),
-    );
+    return Scaffold(
+        appBar: AppBar(title: Text("Comments (${widget.item.descendants})")),
+        body: (smallestWidth >= mediumBreakPoint)
+            ? Align(
+                alignment: Alignment.center,
+                child: Container(width: 1024, child: _getBody()),
+              )
+            : _getBody());
+  }
+
+  Widget _getBody() {
+    return (widget.item.kids.isEmpty)
+        ? ListView(
+            children: <Widget>[
+              HeaderWidget(widget.item),
+              Container(
+                padding: EdgeInsets.all(32.0),
+                child: Center(
+                  child: Text("There are no comments available",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, fontSize: 17.0)),
+                ),
+              )
+            ],
+          )
+        : ListView.builder(
+            itemCount: 1 + widget.item.kids.length,
+            itemBuilder: (BuildContext context, int position) {
+              if (position == 0) {
+                return HeaderWidget(widget.item);
+              }
+
+              return FutureBuilder(
+                  future: getItemWithComments(widget.item.kids[position - 1]),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (comments[position - 1] != null) {
+                      var item = comments[position - 1];
+                      return CommentRow(
+                        item: item,
+                        key: Key(item.id.toString()),
+                      );
+                    }
+                    if (snapshot.hasData && snapshot.data != null) {
+                      var item = snapshot.data;
+                      comments[position - 1] = item;
+                      return CommentRow(
+                          item: item, key: Key(item.id.toString()));
+                    } else if (snapshot.hasError) {
+                      return Container(
+                        padding: EdgeInsets.all(16.0),
+                        child: Center(child: Text("Error Loading Item")),
+                      );
+                    } else {
+                      return Container(
+                        padding: EdgeInsets.all(32.0),
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    }
+                  });
+            },
+          );
   }
 }
